0d30261bba7f14b0088b6f7da5e7ee387ef0ff6d
LaptopUser-PC\Laptop User <gasquarebros@gmail.com>
2018-06-23 18:01:34 +0530

    removing tne testing editor in the file

--- application/modules/home/views/home-index.php
+++ application/modules/home/views/home-index.php
@@ -120,10 +120,7 @@ var module_action="addpost";
 	<p><a class="popup-modal-dismiss" href="#">Dismiss</a></p>
 </div>
 <!--<script type="text/javascript" src="<?php echo skin_url(); ?>js/home.js"></script>-->
-test
-<textarea name="content" id="editor">
-    <p>Here goes the initial content of the editor.</p>
-</textarea>
+
 <script>
 /*  load initial content.. */
 $(window).load(function(){
@@ -163,11 +160,5 @@ $('.draft_post').click(function() {
 	$('#status').val('D');
 	$('#common_form').submit();
 });
-CKEDITOR.replace( 'editor',{
-	filebrowserBrowseUrl: '<?php echo load_lib();?>ckfinder/ckfinder.html',
-    filebrowserUploadUrl: '<?php echo load_lib();?>ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
-    filebrowserWindowWidth: '1000',
-    filebrowserWindowHeight: '700'
-	
-});
+
 </script>
