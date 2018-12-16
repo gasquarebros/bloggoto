983d63452561eedae52be6b58f18ae03db8d1709
LaptopUser-PC\Laptop User <gasquarebros@gmail.com>
2018-12-16 13:56:30 +0530

    logo changes

---
+++ application/logs/log-2018-12-15.php
@@ -0,0 +1,24 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-12-15 12:08:24 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it.
+ D:\xampp\htdocs\bloggotoweb\system\database\drivers\mysqli\mysqli_driver.php 202
+ERROR - 2018-12-15 12:08:24 --> Unable to connect to the database
+ERROR - 2018-12-15 12:08:51 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it.
+ D:\xampp\htdocs\bloggotoweb\system\database\drivers\mysqli\mysqli_driver.php 202
+ERROR - 2018-12-15 12:08:51 --> Unable to connect to the database
+ERROR - 2018-12-15 12:15:50 --> Could not find the language line "select_state"
+ERROR - 2018-12-15 12:18:00 --> Could not find the language line "select_state"
+ERROR - 2018-12-15 12:18:10 --> Could not find the language line "select_state"
+ERROR - 2018-12-15 12:18:25 --> Could not find the language line "select_state"
+ERROR - 2018-12-15 12:18:26 --> Could not find the language line "select_state"
+ERROR - 2018-12-15 12:19:33 --> Could not find the language line "select_state"
+ERROR - 2018-12-15 12:19:47 --> Could not find the language line "select_state"
+ERROR - 2018-12-15 12:20:22 --> Severity: Notice --> Undefined variable: record D:\xampp\htdocs\bloggotoweb\application\modules\services\views\services-views.php 47
+ERROR - 2018-12-15 12:20:22 --> Could not find the language line "select_state"
+ERROR - 2018-12-15 12:20:33 --> Could not find the language line "select_state"
+ERROR - 2018-12-15 12:41:59 --> Could not find the language line "pay"
+ERROR - 2018-12-15 12:41:59 --> Could not find the language line "pay"
+ERROR - 2018-12-15 22:30:46 --> Could not find the language line "select_state"
+ERROR - 2018-12-15 22:52:37 --> Severity: Notice --> Undefined index: product_name D:\xampp\htdocs\bloggotoweb\application\modules\myprofile\views\profile-service-ajax-list.php 445
+ERROR - 2018-12-15 22:52:37 --> Severity: Notice --> Undefined index: product_name D:\xampp\htdocs\bloggotoweb\application\modules\myprofile\views\profile-service-ajax-list.php 445
+ERROR - 2018-12-15 22:52:37 --> Severity: Notice --> Undefined index: product_name D:\xampp\htdocs\bloggotoweb\application\modules\myprofile\views\profile-service-ajax-list.php 445
